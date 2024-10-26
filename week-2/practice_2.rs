94df6fba5aba586be9276038449b946c8c0a4e80
oghenetejirinowumi <owumiteejay@gmail.com>
2024-10-26 11:30:53 -0700

    (My practices and projects)

---
+++ week-2/practice_2.rs
@@ -0,0 +1,11 @@
+fn main () {
+    
+    println!(); // prints just a
+
+    println!("hello ");//prints
+
+    println!("format {} arguements", "some"); //prints format some arguements
+
+    println!("My name is {} I am the class rep of stream 1", "OWUMI OGHENETEJIRIN GREGORY");
+
+}
