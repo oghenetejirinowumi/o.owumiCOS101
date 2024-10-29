119e9864a8ae1a46b31db685653032ce4d1e0fe5
oghenetejirinowumi <owumiteejay@gmail.com>
2024-10-29 03:21:55 -0700

    (My practices for week 3)

---
+++ week-3/practice_10/src/main.rs
@@ -0,0 +1,17 @@
+fn main () {
+    let sum:f64 = 5550.00 + 7310.00; //addition
+    println!("The sum of 5550 and 7310 = {}", sum);
+
+    let difference:f64 = 95.5 - 4.3; // subtraction
+    println!("The difference of 95.5 and 4.3 = {}", difference);
+
+    let product:f64 = 4.0 * 30.0; // multiplication
+    println!("The multiple of 4 and 30 = {}", product);
+
+    let quotient:f64 = 56.7 / 32.2; // division
+    println!("The division of 56.7 and 32.2 = {}", quotient);
+
+    let remainder:f64 = 43.0 % 5.0;
+    println!("The remainder of 42 and 5 = {}", remainder);
+
+}
